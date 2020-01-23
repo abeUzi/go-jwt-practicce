7e5a94948f3277ce37c7ee716aeb3520bb3f2f85
abeUzi <ibanez1111.music@gmail.com>
2020-01-23 18:00:21 +0900

    [add]make client with JWT

---
+++ client/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var mySigningKey = []byte("abasdcasdcebebe")
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GenerateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, validToken)
+}
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "AbeUsi"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("somthing went to wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+func main() {
+	fmt.Println("My simple client")
+
+	handleRequests()
+
+}
